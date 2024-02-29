53c925bc6efa293c462037a46b22c190d67b9bdb
TahaBenHammou <103946547+TahaBenHammou@users.noreply.github.com>
2024-02-29 17:19:09 +0100

    update business entity

--- src/modules/business/entities/business.entity.ts
+++ src/modules/business/entities/business.entity.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const BusinessSchema = new mongoose.Schema({
-    name: { type: String }, 
-    logo: { type: String }, 
+    name: { type: String },
+    logo: { type: String },
     emails: [{ type: String }],
     phoneNumbers: [{ type: String }],
     linkGoogleMap: { type: String },
-    socialLinks: [{ type: String }],
+    socialLinks: [{ type: { type: String },link: { type: String } }],
     whatsappNumbers: [{ type: String }],
     barCode: { type: String },
 },
